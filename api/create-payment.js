3218b326b0d84254e623fcad77a4cbe53e54d40b
Alex Fernando Schissel <77161993+alexfschissel@users.noreply.github.com>
2026-03-31 01:01:29 -0300

    create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,13 +1,12 @@
 const stripe = require(‘stripe’)(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
-// CORS headers
+module.exports = async (req, res) => {
+// CORS
 res.setHeader(‘Access-Control-Allow-Credentials’, ‘true’);
 res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
 res.setHeader(‘Access-Control-Allow-Methods’, ‘GET,OPTIONS,PATCH,DELETE,POST,PUT’);
 res.setHeader(‘Access-Control-Allow-Headers’, ‘X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version’);
 
-// Handle preflight
 if (req.method === ‘OPTIONS’) {
 res.status(200).end();
 return;
@@ -49,4 +48,4 @@ return res.status(500).json({
 error: error.message
 });
 }
-}
+};
